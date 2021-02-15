895c9640b6f172f8082d0fd42687ed08e7498ab4
Aled Parry <aled.skyrail@gmail.com>
2021-02-15 18:50:35 +0000

    Repairing Vue/Webpack/Laravel UI to resolve some errors in Laravel's webpack/mix functionality

--- cs-addressbook/webpack.mix.js
+++ cs-addressbook/webpack.mix.js
@@ -14,4 +14,4 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css')
-    .postCss([ require('tailwindcss') ]);
+    .postCss('resources/css/app.css', 'public/css', [ require('tailwindcss') ]);
