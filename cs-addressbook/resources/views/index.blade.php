2ba4a49420d02c4cc2c0e8c5445c802053d0a4fc
Aled Parry <aled.skyrail@gmail.com>
2021-02-15 18:27:43 +0000

    Mock layout for the contact detail

--- cs-addressbook/resources/views/index.blade.php
+++ cs-addressbook/resources/views/index.blade.php
@@ -11,7 +11,7 @@
 
 </head>
 
-<body class="min-h-screen bg-gray-100 py-6 sm:py-12 justify-center">
+<body class="min-h-screen bg-gray-100 py-6 sm:py-12 flex flex-col justify-center">
     <div class="py-6">
         <div class="relative py-3 lg:w-1/2 md:w-2/3 sm:w-3/4 min-w-max sm:max-w-xl lg:max-w-3xl sm:mx-auto">
             <div class="absolute inset-0 bg-gradient-to-b from-yellow-400 to-yellow-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
@@ -29,7 +29,7 @@
                         </div>
                     </div>
                     <div class="divide-y divide-gray-200 bg-gray-100 rounded-md p-2">
-                        <div class="flex items-center text-black p-4 hover:bg-white">
+                        <div data-contact-id="1" class="flex items-center text-black p-4 hover:bg-white">
                             <img src="https://d2oqjddkfv895o.cloudfront.net/2mCEi8cf/addressbook/contacts/120_rjgZKtQA_thumb.jpg" alt="" class="w-6 h-6 rounded-full mr-2 bg-gray-100">
                             Kevin Francis
                         </div>
@@ -43,7 +43,7 @@
                         </div>
                     </div>
 
-                    <div class="bg-gray-100 rounded-md mt-2 p-2 flex flex-wrap flex-col md:flex-row justify-between">
+                    <div class="bg-gray-100 rounded-md mt-2 p-2 flex flex-wrap-reverse flex-col md:flex-row justify-between">
                         <div class="md:m-0 mb-4 mt-2 text-center">
                             Show
                             <select class="p-2 rounded-md">
@@ -68,6 +68,56 @@
         </div>
     </div>
 
+    <div class="hidden bg-opacity-90 bg-gray-900 fixed left-0 top-0 w-screen h-screen flex flex-col justify-center">
+        <div class="bg-white rounded-lg w-5/6 md:w-2/3 lg:w-1/2 mx-auto p-6">
+            <div class="flex">
+                <img src="https://d2oqjddkfv895o.cloudfront.net/2mCEi8cf/addressbook/contacts/120_rjgZKtQA_thumb.jpg" alt="" class="w-16 h-16 rounded-full mr-2 bg-gray-100">
+                <div class="flex-grow">
+                    <span class="text-lg block">Aled Parry <span class="text-sm">(Mr Aled Wyn Parry)</span></span>
+                    <span class="text-sm block">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 inline-block">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                        </svg>
+                        aledparry@audaciouschurch.com
+                    </span>
+                    <span class="text-sm block">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 inline-block">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" />
+                        </svg>
+                        0788 188 3229
+                    </span>
+                </div>
+            </div>
+            <div class="flex flex-col md:flex-row">
+                <div class="p-4 bg-gray-100 rounded-md mb-2 mt-2 md:m-0 md:mr-2">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 inline-block">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                        </svg>
+                        17th April 1991 (Age 29)
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 inline-block">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+                        </svg>
+                        Production Facilitator at Audacious
+                    </div>
+
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 inline-block">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                        </svg>
+                        Married to Emily Parry
+                    </div>
+                </div>
+                <div class="bg-gray-100 p-4 rounded-md whitespace-pre-line">4 Cromarty Avenue
+                    Chadderton
+                    Oldham
+                    OL9 8AW
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
